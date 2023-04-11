04876436f89dd67638bb4042635581008ee4a551
Joao Santos <joaosantosdev97@gmail.com>
2023-04-11 10:25:44 -0300

    Initialize

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("solidity-coverage");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/<key>",
+      accounts: [],
+    },
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
